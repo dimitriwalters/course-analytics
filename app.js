f59d3c3b8618425dba18cd21bd3bc09b4cb3f58e
Dimitri Walters <me@dimi.io>
2016-05-10 00:53:49 -0400

    Show average

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ app.set('views', 'public');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render('index');
+  var myCursor = mDb.collection('course').aggregate([{
+    $group: {_id: null, average: {$avg: "$grade"}}
+  }], function(err, results) {
+    res.render('index', {average: (results[0] ? results[0].average : 'N/A')});
+  });
 });
 
 app.get('/list', function(req, res) {
